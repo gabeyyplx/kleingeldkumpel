57ba90a09fd364518c609cc1d9d9336b6df00833
gabeyyclear <gabriel.gruenberg@icloud.com>
2024-08-03 11:24:12 +0200

    Add user and account initialization to backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const initialize = require('./utils/init')
 const cors = require('cors')
 const routes = require('./routes/index')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+const main = async () => {
+  await initialize()
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
+}
+
+main()
---
+++ backend/utils/init.js
@@ -0,0 +1,44 @@
+const argon2 = require('argon2')
+const { User, Account } = require('../models')
+
+async function createDefaultUserAndAccount() {
+  try {
+    let userId
+
+    const userCount = await User.count()
+    if (userCount === 0) {
+      const hashedPassword = await argon2.hash('changeme')
+      const user = await User.create({
+        name: 'user',
+        password: hashedPassword,
+      })
+      userId = user.id
+      console.log('Default user created')
+    } else {
+      console.log(
+        `${userCount} user(s) found, skipping creation of default user`
+      )
+    }
+
+    const accountCondition = userId ? { where: { UserId: req.user.id } } : null // Check if user has an account or if there are any accounts at all
+    const accountCount = await Account.count(accountCondition)
+    if (accountCount === 0) {
+      await Account.create({ name: 'Account', balance: 0, userId: userId })
+      console.log('Default account created.')
+    } else {
+      console.log('Valid account found, skipping creation of default account')
+    }
+  } catch (error) {
+    console.error('Error creating default user and account:', error)
+  }
+}
+
+const initFunctions = [createDefaultUserAndAccount]
+
+async function initialize() {
+  for (const initFunction of initFunctions) {
+    await initFunction()
+  }
+}
+
+module.exports = initialize
