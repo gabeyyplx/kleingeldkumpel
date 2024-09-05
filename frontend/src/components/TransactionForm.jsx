98e52daa0d2f81d0a192a90fcf7fc3a503cbe263
gabeyyclear <gabriel.gruenberg@icloud.com>
2024-09-05 18:26:09 +0200

    Add categories to transaction form

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -4,14 +4,38 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Select,
   Switch,
 } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { backendUrl } from '../utils/backend'
 
-function TransactionForm({ onSubmit }) {
+const TransactionForm = ({ onSubmit }) => {
   const [name, setName] = useState('')
   const [amount, setAmount] = useState('')
+  const [category, setCategory] = useState(null)
   const [isExpense, setIsExpense] = useState(true) // Default to expense
+  const [availableCategories, setAvailableCategories] = useState([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(`${backendUrl}/api/categories`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        })
+        if (!response.ok) {
+          throw new Error('Error while fetching categories')
+        }
+        const data = await response.json()
+        setAvailableCategories(data)
+      } catch (error) {
+        console.log(`Error while fetching categories: ${error.messages}`)
+      }
+    }
+    fetchCategories()
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -20,6 +44,7 @@ function TransactionForm({ onSubmit }) {
       name: name,
       value: finalAmount,
       AccountId: 1,
+      CategoryId: category,
     })
   }
 
@@ -45,7 +70,7 @@ function TransactionForm({ onSubmit }) {
         />
       </FormControl>
 
-      <FormControl id='amount' isRequired>
+      <FormControl mb={4} id='amount' isRequired>
         <FormLabel>Amount</FormLabel>
         <Input
           type='number'
@@ -54,6 +79,20 @@ function TransactionForm({ onSubmit }) {
           placeholder='Enter amount'
           step='0.01'
         />
+      </FormControl>
+
+      <FormControl mb={4} id='category' isRequired>
+        <FormLabel>Category</FormLabel>
+        <Select
+          onChange={(e) => setCategory(e.target.value)}
+          placeholder='Choose category'
+        >
+          {availableCategories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.symbol} {category.name}
+            </option>
+          ))}
+        </Select>
       </FormControl>
 
       <Button mt={4} colorScheme={isExpense ? 'red' : 'green'} type='submit'>
